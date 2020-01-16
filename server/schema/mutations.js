5aeed8caf25b9c145f018aa23fe88ab114e90ccd
kmoonwright <kmoonwright@outlook.com>
2020-01-16 13:34:43 -0800

    Refactor mutations

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLList, GraphWLID, GraphQLInt, GraphQLFloat } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLID, GraphQLInt, GraphQLFloat } = graphql;
 const mongoose = require('mongoose');
 const AuthService = require('../services/auth');
 
@@ -23,7 +23,40 @@ const mutations = new GraphQLObjectType({
         return AuthService.register(args);
       }
     },
-
-
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(_, args) {
+        return AuthService.login(args)
+      }
+    },
+    logout: {
+      type: UserType,
+      args: {
+        _id: { type: GraphQLID }
+      }
+    },
+    verifyUser: {
+      type: UserType,
+      args: {
+        token: { type: GraphQLString }
+      },
+      async resolve(_, { name, description, weight }, ctx) {
+        const validUser = await AuthService.verifyUser({ token: ctx.token });
+        // if our service returns true then our item is good to save!
+        // anything else and we'll throw an error
+        if (validUser.loggedIn) {
+          return validUser;
+        } else {
+          throw new Error('Sorry, you need to be logged in to use the website.');
+        }
+      }
+    },
+    
   }
-})
+});
+
+module.exports = mutations;
